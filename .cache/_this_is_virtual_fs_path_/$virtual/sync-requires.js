209c2a13c99cb7059551f974200094c5ef20fc18
vosap2007 <65125720+vosap2007@users.noreply.github.com>
2022-01-17 11:54:07 +0200

    create prog get

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,10 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/volodymyr/Desktop/project/pem-gets/.cache/dev-404-page.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/volodymyr/Desktop/project/pem-gets/src/pages/index.js"))
+}
+
